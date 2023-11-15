0aaa0977766ad2db7361b22745f603ddd8b5834e
Sartaj Bhuvaji <s.bhuvaj@gmail.com>
2023-11-14 22:18:49 -0800

    added Gaussian Blur

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ import time
 import pyautogui
 from directkeys import PressKey, ReleaseKey, W, A, S, D
 
+
+
 def roi(img, vertices):
     mask = np.zeros_like(img)
     cv2.fillPoly(mask, vertices, 255)
@@ -23,10 +25,12 @@ def process_img(original_image):
     # convert to gray
     processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    processed_img = cv2.GaussianBlur(processed_img, (3,3), 0)
     vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500],], np.int32)
     processed_img = roi(processed_img, [vertices])
 
     lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 100, 5)
+    draw_lines(processed_img,lines)
 
     return processed_img
 
