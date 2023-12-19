479f01e77d9b586f5ffec412523a431ea7079d15
Sartaj Bhuvaji <s.bhuvaj@gmail.com>
2023-12-19 15:29:04 -0800

    Refactored check training data

--- v4/check trainingdata.py
+++ v4/check trainingdata.py
@@ -1,22 +1,21 @@
+# Check Training Data
+
 import cv2
 import numpy as np
 import time
 
 training_data = np.load('training_data-1.npy', allow_pickle=True)
-vertices = np.array([[0,25],[0,270],[100,270],[100,200],[430,200],[430,270],[480,270],[480,25],], np.int32)
+mask = False #True
 
-def roi(img, vertices):
-    mask = np.zeros_like(img)   
-    cv2.fillPoly(mask, vertices, color=[255,255,255])
-    masked = cv2.bitwise_and(img, mask)
-    return masked
-
+if mask:
+    vertices = np.array([[0,25],[0,270],[100,270],[100,200],[430,200],[430,270],[480,270],[480,25],], np.int32)
 
 for data in training_data:
     img = data[0]
     choice = data[1]
-    
-    img = roi(img, [vertices])
+
+    if mask:
+        img = roi(img, [vertices])
 
     cv2.imshow('test', img)
     print(choice)
@@ -26,3 +25,9 @@ for data in training_data:
         cv2.destroyAllWindows()
         break
 
+def roi(img, vertices):
+    # Applies ROI Mask to Image
+    mask = np.zeros_like(img)   
+    cv2.fillPoly(mask, vertices, color=[255,255,255])
+    masked = cv2.bitwise_and(img, mask)
+    return masked
