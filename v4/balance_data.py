2bf09a3a6f38aee1e2b7807530c941d55e5924db
Sartaj Bhuvaji <s.bhuvaj@gmail.com>
2023-12-19 15:10:48 -0800

    Adding mask to cover the car

--- v4/balance_data.py
+++ v4/balance_data.py
@@ -5,40 +5,72 @@ import pandas as pd
 from collections import Counter
 from random import shuffle
 
-train_data = np.load('training_data-1.npy')
+train_data = np.load('training_data-1.npy', allow_pickle=True)  
 
 df = pd.DataFrame(train_data)
 print(df.head())
 print(Counter(df[1].apply(str)))
 
-lefts = []
-rights = []
-forwards = []
+w = []
+s =[]
+a = []
+d = []
+wa = []
+wd = []
+sa = []
+sd = []
+nk = []
 
-shuffle(train_data)
+# 'W': [1, 0, 0, 0, 0, 0, 0, 0, 0],
+# 'S': [0, 1, 0, 0, 0, 0, 0, 0, 0],
+# 'A': [0, 0, 1, 0, 0, 0, 0, 0, 0],
+# 'D': [0, 0, 0, 1, 0, 0, 0, 0, 0],
+# 'WA': [0, 0, 0, 0, 1, 0, 0, 0, 0],
+# 'WD': [0, 0, 0, 0, 0, 1, 0, 0, 0],
+# 'SA': [0, 0, 0, 0, 0, 0, 1, 0, 0],
+# 'SD': [0, 0, 0, 0, 0, 0, 0, 1, 0],
+# 'NK': [0, 0, 0, 0, 0, 0, 0, 0, 1],
+# 'default': [0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 for data in train_data:
     img = data[0]
     choice = data[1]
 
-    if choice == [1,0,0]:
-        lefts.append([img,choice])
-    elif choice == [0,1,0]:
-        forwards.append([img,choice])
-    elif choice == [0,0,1]:
-        rights.append([img,choice])
+    if choice == [1, 0, 0, 0, 0, 0, 0, 0, 0]:
+        w.append([img, choice])
+    elif choice == [0, 1, 0, 0, 0, 0, 0, 0, 0]:
+        s.append([img, choice])
+    elif choice == [0, 0, 1, 0, 0, 0, 0, 0, 0]:
+        a.append([img, choice])
+    elif choice == [0, 0, 0, 1, 0, 0, 0, 0, 0]:
+        d.append([img, choice])
+    elif choice == [0, 0, 0, 0, 1, 0, 0, 0, 0]:
+        wa.append([img, choice])
+    elif choice == [0, 0, 0, 0, 0, 1, 0, 0, 0]:
+        wd.append([img, choice])
+    elif choice == [0, 0, 0, 0, 0, 0, 1, 0, 0]:
+        sa.append([img, choice])
+    elif choice == [0, 0, 0, 0, 0, 0, 0, 1, 0]:
+        sd.append([img, choice])
+    elif choice == [0, 0, 0, 0, 0, 0, 0, 0, 1]:
+        nk.append([img, choice])
+    elif choice == [0, 0, 0, 0, 0, 0, 0, 0, 0]:
+        pass    
     else:
-        print('no matches')
+        print('no matches')    
+
+print(len(w), len(s), len(a), len(d), len(wa), len(wd), len(sa), len(sd), len(nk))
 
 
-forwards = forwards[:len(lefts)][:len(rights)]
-lefts = lefts[:len(forwards)]
-rights = rights[:len(forwards)]
 
-final_data = forwards + lefts + rights
-shuffle(final_data)
+# forwards = forwards[:len(lefts)][:len(rights)]
+# lefts = lefts[:len(forwards)]
+# rights = rights[:len(forwards)]
 
-np.save('training_data.npy', final_data)
+# final_data = forwards + lefts + rights
+# shuffle(final_data)
+
+# np.save('training_data.npy', final_data)
 
 
 
