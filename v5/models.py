b7a2fa673155860fc98ff10e35bb21a0acf69339
Sartaj Bhuvaji <s.bhuvaj@gmail.com>
2023-12-31 16:37:11 -0800

    fixed lstm import in models.py

--- v5/models.py
+++ v5/models.py
@@ -2,11 +2,11 @@
 
 import tflearn
 from tflearn.layers.conv import conv_2d, max_pool_2d,avg_pool_2d, conv_3d, max_pool_3d, avg_pool_3d
-from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.core import input_data, dropout, fully_connected, 
 from tflearn.layers.estimator import regression
 from tflearn.layers.normalization import local_response_normalization
 from tflearn.layers.merge_ops import merge
-
+from tflearn.layers.recurrent import lstm
 
 # Enable mixed precision
 # from tensorflow.keras.mixed_precision import experimental as mixed_precision
